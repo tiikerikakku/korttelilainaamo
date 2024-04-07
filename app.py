1e89c61cba8ff0e17223aa8f6646befea4359210
tiikerikakku <146967003+tiikerikakku@users.noreply.github.com>
2024-04-07 18:28:34 +0300

    added app & main page, sign in and out + layout

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+app = Flask(__name__)
+
+app.secret_key = getenv('SECRET')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE')
+db = SQLAlchemy(app)
+
+import routes
